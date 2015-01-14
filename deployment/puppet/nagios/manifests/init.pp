47816859f3afa29ea254528d797de12acbc1c678
Daniele Pizzolli <daniele.pizzolli@create-net.org>
2015-01-14 16:44:28 +0100

    Temporary remove class that gives a lot of problem

--- deployment/puppet/nagios/manifests/init.pp
+++ deployment/puppet/nagios/manifests/init.pp
@@ -23,14 +23,6 @@ $nrpeservice       = $nagios::params::nrpeservice,
     }
     'Debian': {
       $basic_services = ['procs','zombie','swap','user','load','memory']
-      
-      #temp - we will fix	the iso	;)
-      apt::source { 'precise_nagios':
-        location          => 'http://10.20.0.2:8080/ubuntu/fuelweb/x86_64',
-        release           => 'precise',
-        repos             => 'nagios',
-        include_src => false,
-      }
     }
   }
 
@@ -38,75 +30,4 @@ $nrpeservice       = $nagios::params::nrpeservice,
 
   validate_array($services_)
 
-  include nagios::common
-
-  nagios::nrpeconfig { '/etc/nagios/nrpe.cfg':
-    whitelist   => $whitelist,
-    include_dir => "/etc/nagios/${proj_name}",
-  }
-
-  package {$nrpepkg:}
-
-  if inline_template("<%= !(services & ['swift-proxy', 'swift-account',
-    'swift-container', 'swift-object', 'swift-ring']).empty? -%>") == 'true' {
-    package {'nagios-plugins-os-swift':
-      require => Package[$nrpepkg],
-    }
-  }
-
-  if member($services, 'libvirt') == true {
-    package {'nagios-plugins-os-libvirt':
-      require => Package[$nrpepkg],
-    }
-  }
-
-  File {
-    force   => true,
-    purge   => true,
-    recurse => true,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-  }
-
-  file { "/etc/nagios/${proj_name}/openstack.cfg":
-    content => template('nagios/openstack/openstack.cfg.erb'),
-    notify  => Service[$nrpeservice],
-    require => Package[$nrpepkg],
-  }
-
-  file { "/etc/nagios/${proj_name}/commands.cfg":
-    content => template('nagios/common/etc/nagios/nrpe.d/commands.cfg.erb'),
-    notify  => Service[$nrpeservice],
-    require => Package[$nrpepkg],
-  }
-
-  file { "/etc/nagios/${proj_name}":
-    source  => 'puppet:///modules/nagios/common/etc/nagios/nrpe.d',
-    notify  => Service[$nrpeservice],
-    require => Package[$nrpepkg],
-  }
-
-  file { "/usr/local/lib/nagios":
-    mode    => '0755',
-    source  => 'puppet:///modules/nagios/common/usr/local/lib/nagios',
-  }
-
-  firewall { '100 allow nrpe access':
-    port   => [5666],
-    proto  => tcp,
-    action => accept,
-  }
-
-  service {$nrpeservice:
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => false,
-    pattern    => 'nrpe',
-    require    => [
-      File['nrpe.cfg'],
-      Package[$nrpepkg]
-    ],
-  }
 }
