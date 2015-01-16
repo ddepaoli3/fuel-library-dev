d2407cf7b97609fba2cc8f1c80d4fc38e95738e0
Daniele Pizzolli <daniele.pizzolli@create-net.org>
2015-01-16 10:22:55 +0100

    Add workaround to configure puppet to use puppetb

--- deployment/puppet/puppet-351/manifests/init.pp
+++ deployment/puppet/puppet-351/manifests/init.pp
@@ -34,12 +34,34 @@ class puppet-351 {
     require => File["script_install_puppet"],
   }
 
+  # This will not work, we need to update the main part of the conf
+  # While waiting for the bug to be solved we use the ini settings
+  # https://tickets.puppetlabs.com/browse/PDB-1120?jql=project%20%3D%20PDB
+
   class { 'puppetdb::master::config':
     puppetdb_server => '10.20.0.2',  # TODO: no dns/host configured on nodes?
     puppetdb_port   =>  58443,
     restart_puppet  =>  false,  # the default true trows an error:
                                 # Could not find init script or upstart
                                 # conf file for 'puppetmaster'
+    require => Exec["exec_install_puppet"],
+  }
+
+  ini_setting { "workaround for PDB-1120 part 1/2":
+    ensure  => present,
+    path    => '/etc/puppet/puppet.conf',
+    section => 'main',
+    setting => 'storeconfigs_backend',
+    value   => 'puppetdb',
+    require => Exec["exec_install_puppet"],
+  }
+
+  ini_setting { "workaround for PDB-1120 part 2/2":
+    ensure  => present,
+    path    => '/etc/puppet/puppet.conf',
+    section => 'main',
+    setting => 'storeconfigs',
+    value   => 'true',
     require => Exec["exec_install_puppet"],
   }
 
