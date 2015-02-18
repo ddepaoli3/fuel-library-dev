2d9a8e6dd285ff93fbf03a5e62b3287bb9a557ed
Daniel Depaoli <daniel.depaoli@create-net.org>
2015-02-18 17:53:19 +0100

    Fix nagios: * permission of /var/lib/nagios3/rw folder * add www-data to nagios group

--- deployment/puppet/nagios/files/fix_and_run.sh
+++ deployment/puppet/nagios/files/fix_and_run.sh
@@ -6,4 +6,8 @@ chown -R root:nagios /etc/nagios3/
 # This is quite permissive, but needed for www-data to read some
 # unspecified config files
 chmod -R u+rwX,go+rX,go-w /etc/nagios3
+
+usermod -a -G nagios www-data
+chmod g+x /var/lib/nagios3/rw
+
 /etc/init.d/nagios3 restart || true
